7e361f310d0450649950841f33b410cc8be56aba
RachelGcp <rysy3692@gmail.com>
2025-03-05 16:21:51 +0200

    add vocal controller

---
+++ NewDockerApi/Controllers/VocalController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewDockerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VocalController : ControllerBase
+    {
+        // GET: api/<VocalController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<VocalController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<VocalController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VocalController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VocalController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
