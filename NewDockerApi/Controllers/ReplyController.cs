b59e05cf67a0ac395c5f8109e48578f6e121135c
RachelGcp <rysy3692@gmail.com>
2025-04-23 16:42:26 +0300

    add value 3

--- NewDockerApi/Controllers/ReplyController.cs
+++ NewDockerApi/Controllers/ReplyController.cs
@@ -12,7 +12,7 @@ namespace NewDockerApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1" };
+            return new string[] { "value1" ,"value3"};
         }
 
         // GET api/<ReplyController>/5
